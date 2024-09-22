2765ecbd1053ebffcde5e824c107f3e28af18a81
KiraboTraphine <kirabodaphinetraphine@gmail.com>
2024-09-22 12:19:52 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Assign an email address to the variable
+    string email = "kirabodaphinetraphine@gmail.com";
+
+    // Print the final output to the computer screen
+    cout << "My email address is:kirabodaphinetraphine@gmail.com " << email << endl;
+
+    return 0;
+}
+
+
